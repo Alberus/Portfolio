8e8a379c9e6f44e4a3afeb9a5e76df11b8297038
Alberus <42331357+Alberus@users.noreply.github.com>
2022-12-03 14:26:42 -0300

    Resgate game

--- videre/blueprints/pages.py
+++ videre/blueprints/pages.py
@@ -7,6 +7,10 @@ bp = Blueprint('blueprints', __name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
-@bp.route("/historiarum")
+@bp.route("/historiaflix")
 def outra():
     return render_template('historiarum.html')
+
+@bp.route("/resgate")
+def resgate():
+    return render_template('resgate.html')
